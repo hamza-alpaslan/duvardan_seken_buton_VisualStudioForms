3cce676c7dd7dd3cb5e4db28315f204d967122df
User <User@LAPTOP-DLLVIB1A>
2023-03-05 07:35:59 +0300

    Proje dosyası ekle.

---
+++ duvardan_seken/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace duvardan_seken
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            blue = new Button();
+            trackBar1 = new TrackBar();
+            trackBar2 = new TrackBar();
+            timer1 = new System.Windows.Forms.Timer(components);
+            label1 = new Label();
+            label2 = new Label();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)trackBar1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // blue
+            // 
+            blue.Location = new Point(362, 123);
+            blue.Name = "blue";
+            blue.Size = new Size(131, 65);
+            blue.TabIndex = 0;
+            blue.Text = "button1";
+            blue.UseVisualStyleBackColor = true;
+            blue.Click += blue_Click;
+            // 
+            // trackBar1
+            // 
+            trackBar1.Location = new Point(12, 12);
+            trackBar1.Name = "trackBar1";
+            trackBar1.Size = new Size(157, 45);
+            trackBar1.TabIndex = 1;
+            trackBar1.Scroll += trackBar1_Scroll;
+            // 
+            // trackBar2
+            // 
+            trackBar2.Location = new Point(12, 85);
+            trackBar2.Name = "trackBar2";
+            trackBar2.Size = new Size(157, 45);
+            trackBar2.TabIndex = 2;
+            trackBar2.Scroll += trackBar2_Scroll;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 30;
+            timer1.Tick += timer1_Tick;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(175, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(38, 15);
+            label1.TabIndex = 3;
+            label1.Text = "label1";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(175, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(38, 15);
+            label2.TabIndex = 4;
+            label2.Text = "label2";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.ok;
+            pictureBox1.Location = new Point(23, 51);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(33, 28);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 5;
+            pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(pictureBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(trackBar2);
+            Controls.Add(trackBar1);
+            Controls.Add(blue);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)trackBar1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)trackBar2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button blue;
+        private TrackBar trackBar1;
+        private TrackBar trackBar2;
+        private System.Windows.Forms.Timer timer1;
+        private Label label1;
+        private Label label2;
+        private PictureBox pictureBox1;
+    }
+}
---
+++ duvardan_seken/Form1.cs
@@ -0,0 +1,146 @@
+﻿namespace duvardan_seken
+{
+    public partial class Form1 : Form
+    {
+        //https://github.com/hamza-alpaslan
+        // x ve y yönlerindeki hızının ayarlanabilir. düğmeye tıklandığında random_color özelliğinin devreye girip çıktığı,
+        // random_color aktifse arka planın rastgele renk atanır ve bir okla, butonun gittiği yön gösteriliyor
+        public Form1()
+        {
+            InitializeComponent();
+            blue.Text = "RANDOM COLOR(X)";
+        }
+
+        bool randomcolor = false;
+        int sayıx = 1; 
+        int sayıy = 1; 
+        bool yonsag = true;
+        bool yonasagi = true;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Image flipImage = pictureBox1.Image;
+
+            label1.Text = "x hızı=" + sayıx;
+            label2.Text = "y hızı" + sayıy;
+            if (blue.Location.X + blue.Size.Width + 13 >= this.Size.Width)// sağ duvara çarpma kontrolü
+            {
+
+                yonsag = false;
+
+                if (sayıx > 0)//okun sadece sağdan gelip duvara çarptığında döndürülmesi için(taşmalar için)
+                {
+                    flipImage = pictureBox1.Image;
+                    flipImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                    pictureBox1.Image = flipImage;
+                }
+                if (randomcolor == true) renk_degis();
+            }
+            if (blue.Location.Y + blue.Size.Height + 39 >= this.Size.Height)// alt duvara çarpma kontrolü
+            {
+                yonasagi = false;
+                if (sayıy > 0)//okun sadece yukardan gelip duvara çarptığında döndürülmesi için(taşmalar için)
+                {
+                    flipImage = pictureBox1.Image;
+                    flipImage.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                    pictureBox1.Image = flipImage;
+
+                }
+
+                if (randomcolor == true) renk_degis();
+            }
+            if (blue.Location.Y <= 0)// üst duvara çarpma kontrolü
+            {
+                //ust ve sol duvar için taşma kontrolune gerek yok
+                flipImage = pictureBox1.Image;
+                flipImage.RotateFlip(RotateFlipType.RotateNoneFlipY);
+                pictureBox1.Image = flipImage;
+                yonasagi = true;
+
+                if (randomcolor == true) renk_degis();
+            }
+            if (blue.Location.X <= 0)// sol duvara çarpma kontrolü
+            {
+                yonsag = true;
+                flipImage = pictureBox1.Image;
+                flipImage.RotateFlip(RotateFlipType.RotateNoneFlipX);
+                pictureBox1.Image = flipImage;
+
+                if (randomcolor == true) renk_degis();
+            }
+            
+
+            // x ve y hızının negatif-pozitiflik ayarlanması
+            if (yonsag == true)
+            {
+                if (sayıx <= 0)
+                {
+                    sayıx = -sayıx;
+                }
+            }
+
+            if (yonsag == false)
+            {
+                if (sayıx >= 0)
+                {
+                    sayıx = -sayıx;
+                }
+            }
+            if (yonasagi == true)
+            {
+                if (sayıy < 0)
+                {
+                    sayıy = -sayıy;
+                }
+            }
+            if (yonasagi == false)
+            {
+                if (sayıy >= 0)
+                {
+                    sayıy = -sayıy;
+                }
+            }
+
+            blue.Location = new Point(blue.Location.X + sayıx, blue.Location.Y + sayıy);
+
+        }
+        private void renk_degis()
+        {
+            Random rand = new Random();
+            int red = rand.Next() % 256;
+            int green = rand.Next() % 256;
+            int blue = rand.Next() % 256;
+            BackColor = Color.FromArgb(red, green, blue);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            if (yonsag == false) sayıx = -trackBar1.Value;
+            else sayıx = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (yonasagi == false) sayıy = -trackBar2.Value;
+            else sayıy = trackBar2.Value;
+        }
+
+        private void blue_Click(object sender, EventArgs e)
+        {
+            if (randomcolor == false)
+            {
+                blue.Text = "RANDOM COLOR(✓)";
+                randomcolor = true;
+            }
+            else
+            {
+                randomcolor = false;
+                blue.Text = "RANDOM COLOR(X)";
+            }
+        }
+    }
+}
